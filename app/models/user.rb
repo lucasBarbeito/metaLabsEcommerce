5db97b131c35cb6259af86c215855544ff67b815
lucasBarbeito <lucas.barbeito@greencodesoftware.com>
2023-04-30 20:19:16 -0300

    Removes unecesarry comented code

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,5 @@ class User < ApplicationRecord
 
   has_many :carts
   has_many :payments, through: :carts
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 end
